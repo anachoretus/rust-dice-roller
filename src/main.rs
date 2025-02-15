5ffb578c0c74ff68338fdbfd7bdb7e74b1db34b7
Rouven Knape <rouven@knp.de>
2025-02-15 21:37:57 +0100

    taking user input on different die types (d4, d6, d8, d10, d12, d20, and d100)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
 use rand::Rng; //import: random number generator
+use std::io; //import: standard input handling
 
 fn main() {
+    println!("Pick your die:");
+
+    let mut input = String::new(); //store user input in string
+    io::stdin().read_line(&mut input).expect("Failed to read input"); //read input
+
+    let sides: u8 = match input.trim().parse() {
+        Ok(num) if [4, 6, 8, 10, 20, 100].contains(&num) => num, //validate dice sides
+        _ => { //else
+            println!("Invalid input. Defaulting to d20.");
+            20 //default do d20 if input is invalid
+        }
+    };
+
     let mut rng = rand::rng(); // mut = "mutable" => not fixed
-    let roll: u8 =  rng.random_range(1..=20); //generate a number between 1 and 20;
+    let roll: u8 =  rng.random_range(1..=sides); //generate a number between 1 and 20;
     // u8 limits the variable to values between 0 and 255
     println!("You rolled a {}.", roll);
 }
