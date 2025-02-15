b681d1596e5ea47d2370e180b23293c46897742a
Rouven Knape <rouven@knp.de>
2025-02-15 21:23:22 +0100

    creating a dice rolling system in Rust for D&D and other pen & paper sessions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use rand::Rng; //import: random number generator
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::rng(); // mut = "mutable" => not fixed
+    let roll: u8 =  rng.random_range(1..=20); //generate a number between 1 and 20;
+    // u8 limits the variable to values between 0 and 255
+    println!("You rolled a {}.", roll);
 }
